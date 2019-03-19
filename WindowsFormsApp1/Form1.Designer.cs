a97a749824f765dc51861718e8731119176572fb
Hiten <hitendra100282@gmail.com>
2019-03-19 09:11:06 +0530

    1st commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Url = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ResponseTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(22, 86);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.Size = new System.Drawing.Size(209, 115);
+            this.resultsTextBox.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Url,
+            this.ResponseTime,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(268, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(492, 305);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // Url
+            // 
+            this.Url.HeaderText = "Url";
+            this.Url.Name = "Url";
+            this.Url.ReadOnly = true;
+            // 
+            // ResponseTime
+            // 
+            this.ResponseTime.HeaderText = "ResponseTime";
+            this.ResponseTime.Name = "ResponseTime";
+            this.ResponseTime.ReadOnly = true;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void DataGridView1_CellFormatting(object sender, System.Windows.Forms.DataGridViewCellFormattingEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Url;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ResponseTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource cts;
+        Stopwatch sw;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // sw = new Stopwatch();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            resultsTextBox.Clear();
+
+            // Instantiate the CancellationTokenSource.
+            cts = new CancellationTokenSource();
+
+            try
+            {
+                await AccessTheWebAsync(cts.Token);
+                resultsTextBox.Text += "\r\nDownloads complete.";
+            }
+            catch (OperationCanceledException)
+            {
+                resultsTextBox.Text += "\r\nDownloads canceled.\r\n";
+            }
+            catch (Exception)
+            {
+                resultsTextBox.Text += "\r\nDownloads failed.\r\n";
+            }
+
+            cts = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
+        }
+
+        async Task AccessTheWebAsync(CancellationToken ct)
+        {
+            HttpClient client = new HttpClient();
+
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            // ***Create a query that, when executed, returns a collection of tasks.
+            //IEnumerable<Task<int>> downloadTasksQuery =
+            //    from url in urlList select ProcessURL(url, client, ct);
+
+            IEnumerable<Task<Tuple<int,string,string>>> downloadTasksQuery =
+             from url in urlList select ProcessURL(url, client, ct);
+
+            // ***Use ToList to execute the query and start the tasks.
+            //List<Task<int>> downloadTasks = downloadTasksQuery.ToList();
+            List<Task < Tuple<int, string, string> >> downloadTasks = downloadTasksQuery.ToList();
+
+            // ***Add a loop to process the tasks one at a time until none remain.
+            while (downloadTasks.Count > 0)
+            {
+                // Identify the first task that completes.
+                // Task<int> firstFinishedTask = await Task.WhenAny(downloadTasks);
+                Task < Tuple<int, string, string> > firstFinishedTask = await Task.WhenAny(downloadTasks);
+                // ***Remove the selected task from the list so that you don't
+                // process it more than once.
+                downloadTasks.Remove(firstFinishedTask);
+
+                // Await the completed task.
+                //int length = await firstFinishedTask;
+               // int responseCode = firstFinishedTask.Result.Item1;
+                resultsTextBox.Text += $"\r\nLength of the download:  {firstFinishedTask.Result.Item1}";
+                int rowId = dataGridView1.Rows.Add();
+                DataGridViewRow row = dataGridView1.Rows[rowId];
+                row.Cells[0].Value = firstFinishedTask.Result.Item2;
+                row.Cells[1].Value = firstFinishedTask.Result.Item3;
+                row.Cells[2].Value = firstFinishedTask.Result.Item1;
+            }
+        }
+
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                "https://msdn.microsoft.com",
+                "https://google.co.in",
+                "http://yahoo.com",
+                "https://youtube.com",
+                "https://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "http://codeproject.com",
+                "https://msdn.microsoft.com/library/hh290136.aspx",
+                "http://csharp.com",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "http://scotch.io",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "http://www.cricbuzz.com",
+                "https://msdn.microsoft.com/library/ff730837.aspx",
+                "http://desktop-rhvibtv/reports/browse/",
+                "http://localhost/vdi/index.html"
+            };
+            return urls;
+        }
+
+        //async Task<int> ProcessURL(string url, HttpClient client, CancellationToken ct)
+        //{
+        //    // GetAsync returns a Task<HttpResponseMessage>.
+        //    HttpResponseMessage response = await client.GetAsync(url, ct);
+
+        //    // Retrieve the website contents from the HttpResponseMessage.
+        //    byte[] urlContents = await response.Content.ReadAsByteArrayAsync();
+
+        //    return urlContents.Length;
+        //}
+
+        async Task<Tuple<int,string,string>> ProcessURL(string url, HttpClient client, CancellationToken ct)
+        {
+            sw = new Stopwatch();
+            // GetAsync returns a Task<HttpResponseMessage>.
+            sw.Start();
+            HttpResponseMessage response = await client.GetAsync(url, ct);
+            sw.Stop();
+            TimeSpan timeToResponse = sw.Elapsed;
+            // Retrieve the website contents from the HttpResponseMessage.
+            // byte[] urlContents = await response.Content.ReadAsByteArrayAsync();
+
+            return Tuple.Create<int, string, string>((int)response.StatusCode, url, Math.Floor(timeToResponse.TotalMilliseconds).ToString());
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            
+            //if (e.RowIndex != 0)
+            //{
+                if (e.ColumnIndex ==1)
+                {
+                    int amount = Convert.ToInt32(e.Value);                    
+                    // return if rowCount = 0
+                    if (this.dataGridView1.Rows.Count == 0)
+                        return;
+
+                    if (amount > 700)
+                        e.CellStyle.BackColor = Color.Red;
+                    else
+                        e.CellStyle.BackColor = Color.Green;
+                }
+          //  }
+
+        }
+    }
+}
+
